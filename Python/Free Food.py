5d76e36fb1fbd292da2ad526b596436eadb3d832
Nikki Yong <75350534+yongkfn@users.noreply.github.com>
2021-12-20 23:50:44 +0800

    how to make a flat list

---
+++ Python/Free Food.py
@@ -0,0 +1,10 @@
+food = []
+
+for i in range(int(input())):
+    a, b = map(int,input().split())
+    food.append([i for i in range(a,b+1)])
+
+##flat_list = [day for sublist in food for day in sublist]
+##print(len(set(flat_list)))
+
+print(len(set([day for sublist in food for day in sublist])))
