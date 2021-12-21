d3f820bfb673eaa4344a8247ec8d9a988f1cf20a
Nikki Yong <75350534+yongkfn@users.noreply.github.com>
2021-12-21 14:35:27 +0800

    Update Free Food.py

--- Python/Free Food.py
+++ Python/Free Food.py
@@ -4,7 +4,12 @@ for i in range(int(input())):
     a, b = map(int,input().split())
     food.append([i for i in range(a,b+1)])
 
+print(len(set([day for sublist in food for day in sublist])))
+
+
+## More readable
 ##flat_list = [day for sublist in food for day in sublist]
 ##print(len(set(flat_list)))
 
-print(len(set([day for sublist in food for day in sublist])))
+## OR JUST USE .extend()
+
